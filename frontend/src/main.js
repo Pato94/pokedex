5f2f0b7020e5b050c701168dd14ea8fc7a22898c
Gaston Contreras <gastongastongg98@gmail.com>
2020-07-15 16:09:39 -0300

    New register layout

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import Home from './components/Home.vue'
 import PokemonDetail from './components/PokemonDetail.vue'
+import Register from './components/Register.vue'
 
 Vue.use(VueRouter)
 Vue.config.productionTip = false
@@ -11,7 +12,8 @@ const router = new VueRouter({
   mode: 'history',
   routes: [
     { path: '/', component: Home },
-    { name: 'pokemon', path: '/pokemon/:id', component: PokemonDetail }
+    { name: 'pokemon', path: '/pokemon/:id', component: PokemonDetail },
+    { name: 'register', path: '/register', component: Register}
   ]
 })
 
