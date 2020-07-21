0eb39a58e773d872948c7feabbf5e5f20a6d0534
Gaston Contreras <gastongastongg98@gmail.com>
2020-07-21 20:11:59 -0300

    Componente Login y servidor fixed

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import VueRouter from "vue-router";
 import Home from "./components/Home.vue";
 import PokemonDetail from "./components/PokemonDetail.vue";
 import Register from "./components/Register.vue";
+import Login from "./components/Login.vue";
 
 Vue.use(VueRouter);
 Vue.config.productionTip = false;
@@ -14,6 +15,7 @@ const router = new VueRouter({
     { path: "/", component: Home },
     { name: "pokemon", path: "/pokemon/:id", component: PokemonDetail },
     { name: "register", path: "/register", component: Register },
+    { name: "login", path: "/login", component: Login },
   ],
 });
 
